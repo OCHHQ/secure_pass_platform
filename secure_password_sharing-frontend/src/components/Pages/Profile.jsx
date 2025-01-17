a11d4ccc207eae0a3bc42c9128ca7e4917847e41
= <=>
2025-01-17 16:11:56 +0100

    profile page responsive

--- secure_password_sharing-frontend/src/components/Pages/Profile.jsx
+++ secure_password_sharing-frontend/src/components/Pages/Profile.jsx
@@ -1,5 +1,5 @@
 import Header from "../Header/Header"
-import Sidebare from "../Sidebar/Sidebar"
+import Sidebar from "../Sidebar/Sidebar"
 import Footer from "../Footer/Footer"
 import { useUser } from "../Context/UserContext";
 import { useEffect, useState } from "react";
@@ -49,10 +49,10 @@ function Profile() {
     return (
         <>
             <Header />
-            <div className="flex bg-[#F9F6F3]">
-                <Sidebare />
-                <div id='Profile' className='block w-full pt-20 pl-8'>
-                    <div className='h-full rounded-tl-3xl shadow-2xl p-8 bg-[#F9F6F3]'>
+            <div className="flex flex-col md:flex-row bg-[#F9F6F3]">
+                <Sidebar />
+                <div id='Profile' className='flex-1 pt-20 px-4 lg:px-8'>
+                    <div className='h-full rounded-tl-3xl shadow-2xl p-4 lg:p-8 bg-[#F9F6F3]'>
                         {isLoading ? (
                             <div className="flex justify-center items-center h-96">
                                 <p className="text-center">Loading...</p>
@@ -67,8 +67,8 @@ function Profile() {
                                 <input type="text" id="email" className="border-2 border-gray-300 p-2 rounded-lg" value={userProfile.email} name="email" onChange={handleChange} />
                                 <div className="flex flex-col gap-4">
                                     {editPassword ? (
-                                        <div>
-                                            <label htmlFor="password"> Password:</label>
+                                        <div >
+                                            <label htmlFor="password" > Password:</label>
                                             <input type="text" id="password" className="border-2 border-gray-300 p-2 rounded-lg" value={userProfile.password} name="password" onChange={handleChange} />
                                         </div>
                                     ) : (
@@ -79,17 +79,17 @@ function Profile() {
                                 </div>
                             </div>
                         ) : (
-                            <div className='flex flex-col gap-4 m-10'>
+                            <div className='flex flex-col gap-4 m-4 lg:m-10'>
                                 <div className="flex flex-col justify-center items-center">
                                     <FaUserCircle className="w-20 h-20 text-[#7A4FE7]" />
                                     <p className='text-xl font-semibold'>{userProfile.firstname + " " + userProfile.lastname} </p>
                                 </div>
-                                <div className="flex flex-col gap-4 shadow-2xl p-8 bg-[#F9F6F3] rounded-3xl">
+                                <div className="flex flex-col gap-4 shadow-2xl p-4 lg:p-8 bg-[#F9F6F3] rounded-3xl">
                                     <p className='text-xl font-semibold'>Firstname: {userProfile.firstname}</p>
                                     <p className='text-xl font-semibold'>Lastname: {userProfile.lastname}</p>
                                     <p className='text-xl font-semibold'>Email: {userProfile.email}</p>
                                 </div>
-                                <button className="bg-blue-500 hover:bg-blue-700 text-xl text-white font-bold py-2 px-4 rounded-full w-1/2 self-center mt-10" onClick={() => setEdit(true)}>Edit</button>
+                                <button className="bg-blue-500 hover:bg-blue-700 text-xl text-white font-bold py-2 px-4 rounded-full w-full lg:w-1/2 self-center mt-10" onClick={() => setEdit(true)}>Edit</button>
                             </div>
                         ))}
                     </div>
